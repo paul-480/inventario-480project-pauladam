71d673109763265dd76d8a948664dc0365f9e595
Paul Adam <paul.adam@cuatroochenta.com>
2026-04-14 11:40:08 +0200

    Estilos importados de figma

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { ThemeProvider } from "next-themes";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -25,7 +26,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="es" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -33,7 +34,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <ThemeProvider>
         {children}
+        </ThemeProvider>
         <ScrollRestoration />
         <Scripts />
       </body>

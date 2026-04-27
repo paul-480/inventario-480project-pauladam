07a7ca3cea933e2d934c8834114b9ed8c14d6dee
Paul Adam <paul.adam@cuatroochenta.com>
2026-04-27 13:43:59 +0200

    Feature: Manejo de la comunicación con el backend

---
+++ src/infrastructure/api/auth/auth.api.repository.ts
@@ -0,0 +1,19 @@
+import type AuthRepository from "@/infrastructure/auth/auth.repository";
+import { axiosClient } from "../axios.client";
+
+
+export const AuthApiRepository: AuthRepository = {
+    login: async ({ email, password }) => {
+        try {axiosClient.post('/login', { email, password });
+            const response = await axiosClient.post('/login', { email, password });
+            const { token } = response.data;
+            return token;
+        } catch (error) {
+            console.error('Login failed:', error);
+            return null;
+        }
+    },
+    logout: async () => {
+        localStorage.removeItem('token');
+    }
+};

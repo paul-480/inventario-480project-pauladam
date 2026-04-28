33b4a9a285a9e2727dec56b0a6c3fd2ce1dd8af2
Paul Adam <paul.adam@cuatroochenta.com>
2026-04-28 11:38:14 +0200

    Feature: Usuario definido

---
+++ src/domain/user/user.entity.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id:        string;
+    name:      string;
+    surname:   string;
+    email:     string;
+    is_active: boolean;
+    role:      'ADMIN' | 'EMPLOYEE';
+}

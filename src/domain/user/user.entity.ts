4469bb4ff7bfbc2e1fc1aded6c9238579c41564e
Paul Adam <paul.adam@cuatroochenta.com>
2026-04-30 13:55:42 +0200

    Feature: user entity ahora usa user rol global

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,8 +1,10 @@
+import type { UserRole } from "../auth/auth.entity";
+
 export interface User {
     id:        string;
     name:      string;
     surname:   string;
     email:     string;
     is_active: boolean;
-    role:      'ADMIN' | 'EMPLOYEE';
+    role:      UserRole;
 }
